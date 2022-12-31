df3131321d908801bd227c602fcd5d470503d71e
gharjare <gharjarechetan@gmail.com>
2022-12-31 10:59:05 +0530

    [chetan gharjare]program/ add file path here

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StockInventory
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stock Account Management");
+            string stocksFilePath = @"C:\Users\Achal\source\repos\StockInventory\StockInventory\Stock.json";
+            string costemerFilePath = @"C:\Users\Achal\source\repos\StockInventory\StockInventory\Costemer.json";
+            StockAccountManagement management = new StockAccountManagement();
+            management.ReadStockJsonFile(stocksFilePath);
+            management.ReadcostemerJsonFile(costemerFilePath);
+            management.BuyStock("Google");
+            management.SellStock("Google");
+            management.WiteToStockJsonFile(stocksFilePath);
+            management.WiteToCostemerJsonFile(costemerFilePath);
+
+        }
+    }
+}
